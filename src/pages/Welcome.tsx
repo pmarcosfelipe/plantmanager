8c796a9576539ab7c06423ff1618d2d90c9e1bf6
Marcos Felipe <p.marcosfelipe@gmail.com>
2021-04-22 23:32:55 +0100

    add expo-google-fonts and finish welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -6,31 +6,37 @@ import {
   StyleSheet,
   TouchableOpacity,
   Dimensions,
+  View,
 } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 import wateringImage from '../assets/watering.png';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {'\n'} suas plantas {'\n'} de forma fácil
-      </Text>
-
-      <Image source={wateringImage} style={styles.image} redizeMode="contain" />
-
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-        sempre que precisar.
-      </Text>
-
-      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-        <Text style={styles.buttonText}>
-          <Feather name="chevron-right" style={styles.buttonIcon} />
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'} suas plantas de {'\n'} forma fácil
         </Text>
-      </TouchableOpacity>
+
+        <Image
+          source={wateringImage}
+          style={styles.image}
+          redizeMode="contain"
+        />
+
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+          sempre que precisar.
+        </Text>
+
+        <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+          <Feather name="chevron-right" style={styles.buttonIcon} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -38,10 +44,16 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'space-around',
+    paddingHorizontal: 20,
   },
   title: {
+    fontFamily: fonts.heading,
+    lineHeight: 34,
     fontSize: 32,
     fontWeight: 'bold',
     textAlign: 'center',
@@ -49,6 +61,7 @@ const styles = StyleSheet.create({
     marginTop: 38,
   },
   subtitle: {
+    fontFamily: fonts.text,
     fontSize: 18,
     textAlign: 'center',
     paddingHorizontal: 20,
@@ -66,11 +79,8 @@ const styles = StyleSheet.create({
     width: 56,
     height: 56,
   },
-  buttonText: {
-    fontSize: 16,
-    color: colors.white,
-  },
   buttonIcon: {
     fontSize: 32,
+    color: colors.white,
   },
 });
