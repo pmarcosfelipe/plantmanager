e946b566de7e3606cb1061080362a587a1a8b107
Marcos Felipe <p.marcosfelipe@gmail.com>
2021-05-09 18:18:28 +0100

    wip create swipeable button to delete plant

--- src/components/PlantCardSecondary.tsx
+++ src/components/PlantCardSecondary.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { Text, StyleSheet, View, Animated } from 'react-native';
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 import { SvgFromUri } from 'react-native-svg';
 
 import colors from '../styles/colors';
@@ -12,18 +14,36 @@ interface PlantProps extends RectButtonProps {
     photo: string;
     hour: string;
   };
+  handleRemove: () => void;
 }
 
 export const PlantCardSecondary = ({ data, ...rest }: PlantProps) => {
+  function handleRemove() {
+    console.log('remove');
+  }
+
   return (
-    <RectButton style={styles.container} {...rest}>
-      <SvgFromUri uri={data.photo} width={50} height={50} />
-      <Text style={styles.title}>{data.name}</Text>
-      <View style={styles.details}>
-        <Text style={styles.timeLabel}>Regar às</Text>
-        <Text style={styles.time}>{data.hour}</Text>
-      </View>
-    </RectButton>
+    <Swipeable
+      overshootRight={false}
+      renderRightActions={() => (
+        <Animated.View>
+          <View>
+            <RectButton style={styles.buttonRemove} onPress={handleRemove}>
+              <Feather name="trash" size={32} color={colors.white} />
+            </RectButton>
+          </View>
+        </Animated.View>
+      )}
+    >
+      <RectButton style={styles.container} {...rest}>
+        <SvgFromUri uri={data.photo} width={50} height={50} />
+        <Text style={styles.title}>{data.name}</Text>
+        <View style={styles.details}>
+          <Text style={styles.timeLabel}>Regar às</Text>
+          <Text style={styles.time}>{data.hour}</Text>
+        </View>
+      </RectButton>
+    </Swipeable>
   );
 };
 
@@ -58,5 +78,17 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontFamily: fonts.heading,
     color: colors.body_dark,
+  },
+  buttonRemove: {
+    marginVertical: 15,
+    width: 100,
+    height: 70,
+    backgroundColor: colors.red,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    right: 15,
+    position: 'relative',
+    paddingLeft: 10,
   },
 });
